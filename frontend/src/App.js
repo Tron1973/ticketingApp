a272d5fff7cb1ae6ff6473420af57d878683fb4b
Ryan Wilson <ryanellsworthwilson@gmail.com>
2022-10-31 14:43:35 -0700

    added apiSlice file and added routes to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,30 @@ import Public from './components/Public'
 import Login from './features/auth/Login'
 import DashLayout from './components/DashLayout'
 import Welcome from './features/auth/Welcome'
+import NotesList from './features/notes/NotesList'
+import UsersList from './features/users/UsersList'
 
 function App() {
   return (
     <Routes>
+
       <Route path='/' element={<Layout />} >
         <Route index element={<Public />} />
         <Route path='/login' element={<Login />} />
 
-        <Route path='dash' element={<DashLayout />} >
+        <Route path='dash' element={<DashLayout />} >{/* Dashboard starts here */}
 
-        <Route index element={<Welcome />} />
+          <Route index element={<Welcome />} />
+
+          <Route path='notes'>
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path='users'>
+            <Route index element={<UsersList />} />
+          </Route>
         
-        </Route>
+        </Route>{/* End of the dashboard */}
 
       </Route>
     </Routes>
