2e1d08c9d1ef64776a8c25bae64bc60067c85b49
Ryan Wilson <ryanellsworthwilson@gmail.com>
2022-11-01 14:15:39 -0700

    added apiSlice files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
 
@@ -8,10 +10,12 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
