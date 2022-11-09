7d66d56529f82284f0ddc70d26defe5cba51c03a
Ryan Wilson <ryanellsworthwilson@gmail.com>
2022-11-09 14:12:36 -0700

    added prefetch and listeners

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import EditUser from './features/users/EditUser'
 import NewUserForm from './features/users/NewUserForm'
 import EditNote from './features/notes/EditNote'
 import NewNote from './features/notes/NewNote'
+import Prefetch from './features/auth/Prefetch'
 
 function App() {
   return (
@@ -19,23 +20,25 @@ function App() {
         <Route index element={<Public />} />
         <Route path='/login' element={<Login />} />
 
-        <Route path='dash' element={<DashLayout />} >{/* Dashboard starts here */}
+        <Route element={<Prefetch />} >
+          <Route path='dash' element={<DashLayout />} >{/* Dashboard starts here */}
 
-          <Route index element={<Welcome />} />
+            <Route index element={<Welcome />} />
 
-          <Route path='users'>
-            <Route index element={<UsersList />} />
-            <Route path=':id' element={<EditUser />} />
-            <Route path='new' element={<NewUserForm />} />
-          </Route>
+            <Route path='users'>
+              <Route index element={<UsersList />} />
+              <Route path=':id' element={<EditUser />} />
+              <Route path='new' element={<NewUserForm />} />
+            </Route>
 
-          <Route path='notes'>
-            <Route index element={<NotesList />} />
-            <Route path=':id' element={<EditNote />} />
-            <Route path='new' element={<NewNote />} />
-          </Route>
-        
-        </Route>{/* End of the dashboard */}
+            <Route path='notes'>
+              <Route index element={<NotesList />} />
+              <Route path=':id' element={<EditNote />} />
+              <Route path='new' element={<NewNote />} />
+            </Route>
+          
+          </Route>{/* End of the dashboard */}
+        </Route>
 
       </Route>
     </Routes>
