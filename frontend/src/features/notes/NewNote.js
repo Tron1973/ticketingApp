8d301ae7c7fd31df39e56e962362d0076df3df88
Ryan Wilson <ryanellsworthwilson@gmail.com>
2022-12-13 15:56:39 -0700

    fixed store import and newNote error

--- frontend/src/features/notes/NewNote.js
+++ frontend/src/features/notes/NewNote.js
@@ -6,7 +6,7 @@ import NewNoteForm from './NewNoteForm'
 const NewNote = () => {
   const users = useSelector(selectAllUsers)
 
-  if (!users.length) return <p>Not Currently Available</p>
+  if (!users?.length) return <p>Not Currently Available</p>
 
   const content = <NewNoteForm users={users} />
 
