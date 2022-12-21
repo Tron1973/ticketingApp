160b0649b45bba2c60a25c80eb7ca2046bcd191c
Ryan Wilson <ryanellsworthwilson@gmail.com>
2022-12-21 12:46:20 -0700

    fixed express async error handling in backend files

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,23 +1,22 @@
 const User = require('../models/User')
 const Note = require('../models/Note')
-const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
 
 // @desc Get all users
 // @route GET /users
 // @access Private
-const getAllUsers = asyncHandler(async (req, res) => {
+const getAllUsers = async (req, res) => {
   const users = await User.find().select('-password').lean()
   if (!users?.length) {
     return res.status(400).json({ message: 'No users found' })
   }
   res.json(users)
-})
+}
 
 // @desc Create new user
 // @route POST /users
 // @access Private
-const createNewUser = asyncHandler(async (req, res) => {
+const createNewUser = async (req, res) => {
   const { username, password, roles } = req.body
 
   // Confirm data
@@ -43,12 +42,12 @@ const createNewUser = asyncHandler(async (req, res) => {
   } else {
     res.status(400).json({ message: 'Invalid user data received'})
   }
-})
+}
 
 // @desc Update a user
 // @route PATCH /users
 // @access Private
-const updateUser = asyncHandler(async (req, res) => {
+const updateUser = async (req, res) => {
   const { id, username, roles, active, password } = req.body
 
   // Confirm data
@@ -80,12 +79,12 @@ const updateUser = asyncHandler(async (req, res) => {
   const updatedUser = await user.save()
 
   res.json({ message: `${updatedUser.username} updated` })
-})
+}
 
 // @desc Delete a user
 // @route DELETE /users
 // @access Private
-const deleteUser = asyncHandler(async (req, res) => {
+const deleteUser = async (req, res) => {
   const { id } = req.body
 
   if (!id) {
@@ -108,7 +107,7 @@ const deleteUser = asyncHandler(async (req, res) => {
   const reply = `Username ${result.username} with ID ${result._id} deleted`
 
   res.json(reply)
-})
+}
 
 module.exports = {
   getAllUsers,
--- backend/middleware/loginLimiter.js
+++ backend/middleware/loginLimiter.js
@@ -2,15 +2,15 @@ const rateLimit = require('express-rate-limit')
 const { logEvents } = require('./logger')
 
 const loginLimiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 5, // limit each IP address to 5 login requests per window per minute
+  windowMs: 60 * 1000,
+  max: 5,
   message: { message: 'Too many login attempts from this IP address, please try again in 60 seconds'},
   handler: (req, res, next, options) => {
     logEvents(`Too Many Requests: ${options.message.message}\t${req.mthod}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
     res.status(options.statusCode).send(options.message)
   },
-  standardHeaders: true, // return rate limit info in `RateLimit` headers
-  legacyHeaders: false, // disable the `X-RateLimit` headers
+  standardHeaders: true,
+  legacyHeaders: false,
 })
 
 module.exports = loginLimiter
