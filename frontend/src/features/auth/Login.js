9a581a3d1a9f6c1bbcb4d29fdce603910c763591
Ryan Wilson <ryanellsworthwilson@gmail.com>
2023-01-29 11:37:55 -0700

    changed Note query structure

--- frontend/src/features/auth/Login.js
+++ frontend/src/features/auth/Login.js
@@ -66,7 +66,7 @@ const Login = () => {
         <form className="form" onSubmit={handleSubmit}>
           <label htmlFor="username">Username:</label>
           <input className="form__input" type="text" id="username" ref={userRef} value={username} onChange={handleUserInput} autoComplete="off" required />
-          <label htmlFor="password">Username:</label>
+          <label htmlFor="password">Password:</label>
           <input className="form__input" type="password" id="password" value={password} onChange={handlePwdInput} required />
           <button className="form__submit-button">Sign In</button>
           <label htmlFor="persist" className="form__persist">
